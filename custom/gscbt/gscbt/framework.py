vol scaler bug fix: inf in initial calculation

custom/gscbt/gscbt/framework.py
@@ -4,6 +4,43 @@ from numba import jit
 from scipy.optimize import minimize
 
 
+# def volatility_scalar(
+#     price_df,
+#     return_callable,
+#     ticker,
+#     unit_cash_volatility_target,
+#     span=None,
+# ):
+#     temp = pd.DataFrame()
+#     temp["unit_returns"] = return_callable(price_df)  # *100
+#     temp["unit_returns"] = temp["unit_returns"].fillna(0)
+
+#     _vol_fast = temp["unit_returns"].ewm(span=span).std()
+#     _vol_slow = temp["unit_returns"].ewm(halflife=span * 2).std()
+
+#     temp["unit_volatility"] = pd.Series(
+#         np.where(
+#             _vol_fast < _vol_slow * 0.30,
+#             _vol_slow,
+#             _vol_fast,
+#         ),
+#         index=_vol_fast.index,
+#     )
+#     # temp["unit_volatility"] = temp["unit_returns"].ewm(span = span).std()
+
+#     temp["block_value"] = (
+#         price_df.underlying[ticker.symbol.lower()] * ticker.currency_multiplier
+#     )
+#     temp["instrument_currency_volatility"] = (
+#         temp["unit_volatility"] * temp["block_value"]
+#     )
+#     temp["volatility_scalar"] = (
+#         unit_cash_volatility_target / temp["instrument_currency_volatility"]
+#     )
+#     temp["volatility_scalar"] = temp["volatility_scalar"].shift(1)
+
+#     return temp[["volatility_scalar"]].squeeze()
+
 @jit(forceobj=True)
 def volatility_scalar(
     price_df,
@@ -11,48 +48,35 @@ def volatility_scalar(
     ticker,
     unit_cash_volatility_target,
     span=None,
-    print_info=False,
 ):
+
     temp = pd.DataFrame()
     temp["unit_returns"] = return_callable(price_df)
     temp["unit_returns"] = temp["unit_returns"].fillna(0)
-
     temp["unit_volatility"] = temp["unit_returns"].ewm(span=span).std()
 
     n = len(temp)
-
     underlying_price = price_df.underlying[ticker.symbol.lower()].values
     unit_vol = temp["unit_volatility"].values
     equal_price_counter = np.zeros(n, dtype=int)
-
     vol_resume = span
-    # start = 0
-    # end = 0
 
     for i in range(1, n):
         if underlying_price[i] == underlying_price[i - 1]:
             equal_price_counter[i] = equal_price_counter[i - 1] + 1
-
-        # if equal_price_counter[i] == span:
-        #     start = i - span
-        #     if start <= 0:
-        #         start = 0
-
         if equal_price_counter[i] >= span:
             copy_vol_idx = int(i - equal_price_counter[i])
             if copy_vol_idx >= 0:
                 unit_vol[i] = unit_vol[copy_vol_idx]
-
         if equal_price_counter[i] == 0 and equal_price_counter[i - 1] >= span:
-            # end = i
-            # if print_info:
-            #     print(f"{temp.index[start]} : {temp.index[end]} - {end-start}")
             for j in range(vol_resume):
                 if i + j < n:
                     unit_vol[i + j] = unit_vol[i - 1]
-
+                    
+    for i in range(1, n):
+        if unit_vol[i] == 0:
+            unit_vol[i] = np.nan
     temp["unit_volatility"] = unit_vol
-
     temp["block_value"] = (
         price_df.underlying[ticker.symbol.lower()] * ticker.currency_multiplier
     )
@@ -63,8 +87,8 @@ def volatility_scalar(
         unit_cash_volatility_target / temp["instrument_currency_volatility"]
     )
     temp["volatility_scalar"] = temp["volatility_scalar"].shift(1)
-
     return temp[["volatility_scalar"]].squeeze()
+
 
 def correlation(returns):
     return returns.corr().clip(lower=0)

src/gscap/framework/strategy/analyse.py
@@ -1,18 +1,19 @@
 from __future__ import annotations
-from prettytable import PrettyTable
-import gscap
-from gscap.metrics import tail_ratios, drawdown_series
-from gscap import plot
-import numpy as np
-
 
 from typing import TYPE_CHECKING
+
+import numpy as np
+from prettytable import PrettyTable
+
+import gscap
+from gscap import plot
+from gscap.metrics import drawdown_series, tail_ratios
 
 if TYPE_CHECKING:
     from gscap.framework.strategy import Strategy
 
 
-def _analyse(main_strat: Strategy, benchmark: Strategy = None, show=True):
+def _strategy_plots(main_strat: Strategy, benchmark: Strategy = None, show=True):
     main_rs = main_strat.aggr_return_series
     bench_rs = benchmark.aggr_return_series if benchmark is not None else None
     plot.returns(main_rs, benchmark=bench_rs, cumulative=False, show=show)
@@ -33,13 +34,19 @@ def stats(strat: Strategy):
     yearly_rtr = strat.aggr_return_series.resample("YE").sum().dropna()
     annualized_daily_rtr = daily_rtr.mean() * gscap.DAYS_IN_YEAR
     annualized_daily_std = daily_rtr.std() * np.sqrt(gscap.DAYS_IN_YEAR)
+    daily_sharpe = annualized_daily_rtr / annualized_daily_std
+
+    annualized_monthly_rtr = monthly_rtr.mean() * 12
+    annualized_monthly_std = monthly_rtr.std() * np.sqrt(12)
+    monthly_sharpe = annualized_monthly_rtr / annualized_monthly_std
+
     dd = drawdown_series(daily_rtr, cumulative=True)
     max_dd = dd.min() * 100
     mean_dd = dd.mean() * 100
-    sharpe = annualized_daily_rtr / annualized_daily_std
     tratios_daily = tail_ratios(daily_rtr)
     tratios_monthly = tail_ratios(monthly_rtr)
-    skew = daily_rtr.skew()
+    daily_skew = daily_rtr.skew()
+    monthly_skew = monthly_rtr.skew()
     expected_daily_rtr = daily_rtr.mean() * 100
     expected_monthly_rtr = monthly_rtr.mean() * 100
     expected_yearly_rtr = yearly_rtr.mean() * 100
@@ -64,15 +71,21 @@ def stats(strat: Strategy):
     win_years = yearly_rtr[yearly_rtr > 0].count() / len(yearly_rtr) * 100
 
     return {
-        "top": {
+        "top00": {
             "Annualized Daily Return": annualized_daily_rtr * 100,
             "Annualized Daily Volatility": annualized_daily_std * 100,
-            "Annualized Sharpe Ratio": sharpe,
+            "Annualized Daily SR": daily_sharpe,
+        },
+        "top01": {
+            "Annualized Monthly Return": annualized_monthly_rtr * 100,
+            "Annualized Monthly Volatility": annualized_monthly_std * 100,
+            "Annualized Monthly SR": monthly_sharpe,
         },
         "risk": {
             "Max Drawdown": max_dd,
             "Mean Drawdown": mean_dd,
-            "Skew (Daily Return)": skew,
+            "Skew (Daily Return)": daily_skew,
+            "Skew (Monthly Return)": monthly_skew,
         },
         "skew_kurt": {
             "Left Tail Ratio (Daily)": tratios_daily["left"],