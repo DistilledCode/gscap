cost calculation & correct trading hours: HE, LE, GF

src/gscap/framework/strategy/__init__.py
@@ -7,15 +7,13 @@ import pandas as pd
 from gscbt.utils import Dotdict
 
 import gscap
-from gscap import plot
+# from gscap import plot
 from gscap.framework.forecast import Forecast
 from gscap.framework.instruments import Instrument
-
 # import matplotlib.pyplot as plt
 from gscap.framework.strategy.analyse import _metric_table, _strategy_plots
+from gscap.framework.strategy.calculations import buffer, calculate_idm
 from gscap.framework.subsystem import SubSystem
-from gscap.framework.utils import calculate_idm
-from gscap.utils import buffer
 
 
 def instrument_weight(returns: pd.DataFrame, resample="YE", n_itr=100, frac=0.1):
@@ -40,20 +38,22 @@ class Strategy:
         risk_weights=None,
         buffer_fraction: Optional[float] = None,
         name: Optional[str] = "strategy_xx",
+        include_cost: bool = True,
     ):
         self.name = name
         self.contracts = contracts
         self.forecasts = forecasts
         self.capital = capital
         self.tau = tau
-        self.yearly_cvt = self.capital * self.tau
+        self.annual_cvt = self.capital * self.tau
         self.interval = interval
         self.period = period
-        self.fmapping = None
-        self.subsystems = None
         self.fdm_resample = gscap.FDM_RESAMPLE if fdm_resample is None else fdm_resample
         self.risk_weights = risk_weights
         self.buffer_fraction = buffer_fraction
+        self.include_cost = include_cost
+        self.fmapping = None
+        self.subsystems = None
         self.indv_return_series: pd.DataFrame = None
         self.aggr_return_series: pd.Series = None
         self.instrument_weights: pd.DataFrame = None
@@ -61,56 +61,22 @@ class Strategy:
 
         if self.interval == "5m":
             gscap.VOL_LOOKBACK_SPAN = 12
-            self.unit_cvt = self.yearly_cvt / np.sqrt(gscap.MINUTES_IN_YEAR / 5)
         elif self.interval == "1d":
             gscap.VOL_LOOKBACK_SPAN = 5
-            self.unit_cvt = self.yearly_cvt / np.sqrt(gscap.DAYS_IN_YEAR)
 
     def init(self):
 
-        if isinstance(self.forecasts, dict) and self.contracts is None:
-            self.fmapping = {
-                Instrument(
-                    meta=cnt_meta,
-                    period=self.period,
-                    interval=self.interval,
-                ): fcast
-                for cnt_meta, fcast in self.forecasts.items()
-            }
-        elif not isinstance(self.forecasts, dict) and self.contracts is not None:
-            self.fmapping = {
-                Instrument(
-                    meta=cnt_meta,
-                    period=self.period,
-                    interval=self.interval,
-                ): self.forecasts
-                for cnt_meta in self.contracts
-            }
-        else:
-            raise ValueError(
-                "If `forecasts` is dict then `contracts` must be None.",
-                "If `contracts` is not None then `forecasts` cannot be dict;"
-                "it must be `Forecast | list[Forecast]`",
-            )
+        self._process_fmapping()
+        self._sanity_check()
 
-        for inst in self.fmapping:
-            if any(
-                (
-                    np.isnan(inst.meta.dollar_equivalent) is np.True_,
-                    np.isnan(inst.meta.currency_multiplier) is np.True_,
-                )
-            ):
-                raise AttributeError(
-                    f"NaN values for multipliers of {inst}: "
-                    f"{inst.meta.dollar_equivalent=}; {inst.meta.currency_multiplier=}"
-                )
         self.subsystems = [
             SubSystem(
-                instruement=inst,
+                instrument=inst,
                 forecasts=fcasts,
-                unit_cash_vol_tgt=self.unit_cvt,
+                annual_cash_vol_tgt=self.annual_cvt,
                 fdm_resample=self.fdm_resample,
                 capital=self.capital,
+                include_cost=self.include_cost,
             )
             for inst, fcasts in self.fmapping.items()
         ]
@@ -134,6 +100,7 @@ class Strategy:
             if self.buffer_fraction:
                 ss.position = buffer(ss.position, fraction=self.buffer_fraction)
         self.positions = pd.concat((ss.position for ss in self.subsystems), axis=1)
+        self.positions = self.positions.round()
 
     def _calculate_ss_return_series(self):
         for ss in self.subsystems:
@@ -149,7 +116,11 @@ class Strategy:
         self.instrument_weights = instrument_weight(self.indv_return_series)
 
     def _calculate_idm(self):
-        self.idm = calculate_idm(self.indv_return_series, self.instrument_weights)
+        if len(self.fmapping) > 1:
+            self.idm = calculate_idm(self.indv_return_series, self.instrument_weights)
+        elif len(self.fmapping) == 1:
+            self.idm = pd.Series(1.0, index=self.instrument_weights.index)
+
         self.idm.name = self.name
 
     def compile(self):
@@ -178,5 +149,60 @@ class Strategy:
         if not isinstance(other_strategy, Strategy):
             raise ValueError("Pass another `Strategy` instance for comaparison")
 
+        if self.name == other_strategy.name:
+            _err = f"Conflicting Strategy names: {repr(self.name)} & {repr(other_strategy.name)}"
+            raise RuntimeError(_err)
         _strategy_plots(self, benchmark=other_strategy, show=show)
         _metric_table(self, benchmark=other_strategy)
+
+    def _process_fmapping(self):
+
+        if isinstance(self.forecasts, dict) and self.contracts is None:
+            self.fmapping = {
+                Instrument(
+                    meta=cnt_meta,
+                    period=self.period,
+                    interval=self.interval,
+                ): fcast
+                for cnt_meta, fcast in self.forecasts.items()
+            }
+        elif not isinstance(self.forecasts, dict) and self.contracts is not None:
+            self.fmapping = {
+                Instrument(
+                    meta=cnt_meta,
+                    period=self.period,
+                    interval=self.interval,
+                ): self.forecasts
+                for cnt_meta in self.contracts
+            }
+        else:
+            raise ValueError(
+                "If `forecasts` is dict then `contracts` must be None.",
+                "If `contracts` is not None then `forecasts` cannot be dict;"
+                "it must be `Forecast | list[Forecast]`",
+            )
+
+    def _sanity_check(self):
+        for inst in self.fmapping:
+            if any(
+                (
+                    np.isnan(inst.meta.dollar_equivalent) is np.True_,
+                    np.isnan(inst.meta.currency_multiplier) is np.True_,
+                )
+            ):
+                raise AttributeError(
+                    f"NaN values for multipliers of {inst}: "
+                    f"{inst.meta.dollar_equivalent=}; {inst.meta.currency_multiplier=}"
+                )
+            if np.isnan(inst.meta.currency_tick_value) is np.True_:
+                raise AttributeError(f"NaN values for tick value of {inst}")
+
+        _symbols = [instrmnt.meta.symbol for instrmnt in self.fmapping]
+        assert len(set(_symbols)) == len(_symbols), "Symbols not unique!"
+
+        if isinstance(self.buffer_fraction, bool):
+            _str = (
+                f"Invalid type for {type(self.buffer_fraction)=}; ",
+                f"Can only be `None` or `float`",
+            )
+            raise ValueError(_str)

src/gscap/framework/subsystem/__init__.py
@@ -1,10 +1,13 @@
+import numpy as np
 import pandas as pd
 from gscbt.framework import volatility_scalar
 
 import gscap
 from gscap.framework.forecast import Forecast
 from gscap.framework.instruments import Instrument
-from gscap.utils import percentage_returns_series
+from gscap.framework.subsystem.calculations import (percentage_returns_series,
+                                                    prs_with_cost)
+from gscap.framework.utils import Cost, get_th_series
 
 
 def get_returns(price_df=pd.DataFrame):
@@ -14,24 +17,37 @@ def get_returns(price_df=pd.DataFrame):
 class SubSystem:
     def __init__(
         self,
-        instruement: Instrument,
+        instrument: Instrument,
         forecasts: list[Forecast] | Forecast,
-        unit_cash_vol_tgt: float,
+        annual_cash_vol_tgt: float,
         capital: float | int = 1_000_000,
         fdm_resample="B",
+        include_cost: bool = True,
     ):
-        self.instrument = instruement
+        self.instrument = instrument.clone()
         self.forecasts = (
             [i.clone() for i in forecasts]
             if isinstance(forecasts, list)
             else forecasts.clone()
         )
-        self.unit_cv_target = unit_cash_vol_tgt
+        self.annual_cv_target = annual_cash_vol_tgt
         self.fdm_resample = fdm_resample
         self.capital = capital
+        self.include_cost = include_cost
         self._volatility_scalar = None
         self._position = None
         self.return_series = None
+        self.slippage_cost_currency = None
+        self.commission_cost_currency = None
+        self.risk_adj_cost_per_lot = None
+        self.cost = Cost()
+        if self.instrument.interval == "5m":
+            if self.instrument.meta.trading_hours < 0:
+                self.instrument.meta.trading_hours = get_th_series(self.instrument)
+            intv_num = gscap.DAYS_IN_YEAR * self.instrument.meta.trading_hours * 60 / 5
+        elif self.instrument.interval == "1d":
+            intv_num = gscap.DAYS_IN_YEAR
+        self.unit_cv_target = self.annual_cv_target / np.sqrt(intv_num)
 
     @property
     def volatility_scalar(self) -> pd.Series:
@@ -48,6 +64,18 @@ class SubSystem:
 
     @property
     def position(self) -> pd.Series:
+        """
+        # Regarding rounding off
+        - SubSystem positions will stay unrounded as buffering requires unrounded positions
+        - If the Strategy class doesn't buffer the positions then SubSystem postions will
+            stay unrouded, else they'll be rounded
+        - Positions of all instrument in Strategy attribute `positions` are rounded
+        - For return_series calculation, we'll explicitly round the postion inside the func
+
+        # Regarding scaling with risk weights
+        - in `_calculate_ss_positions()` method of Strategy the  `.position` attribute of
+            SubSystem get scaled down with the risk weight.
+        """
         if self._position is None:
             if self.instrument.cfs is None:
                 self._process_forecasts()
@@ -62,7 +90,6 @@ class SubSystem:
 
     @position.setter
     def position(self, value: pd.Series) -> None:
-
         self._position = value
         self._position.name = self.instrument.meta.symbol.lower()
 
@@ -72,13 +99,17 @@ class SubSystem:
         self.instrument.combine_forecast(resample=self.fdm_resample)
 
     def calculate_return_series(self) -> pd.Series:
-        _prs = percentage_returns_series(
-            self.position,
-            self.instrument.close_price().adjusted,
-            multiplier=self.instrument.meta.dollar_equivalent,
-            capital_series=self.capital,
-            fx_series=None,
-        )
+        if self.include_cost is False:
+            _prs = percentage_returns_series(
+                self.position,
+                self.instrument.close_price().adjusted,
+                multiplier=self.instrument.meta.dollar_equivalent,
+                capital_series=self.capital,
+                fx_series=None,
+            )
+            self._set_cost_zero()
+        else:
+            _prs = prs_with_cost(self, capital_series=self.capital, fx_series=None)
         self.return_series = _prs.fillna(0)
         self.return_series.name = self.instrument.meta.symbol.lower()
         return self.return_series
@@ -89,3 +120,10 @@ class SubSystem:
             f"{self.instrument}, {self.instrument.interval}, "
             f"{self.instrument.period}, {_n}"
         )
+
+    def _set_cost_zero(self):
+        self.cost.slippage_currency = pd.Series(0.0, index=self.position.index)
+        self.cost.commission_currency = pd.Series(0.0, index=self.position.index)
+        self.cost.total_currency = pd.Series(0.0, index=self.position.index)
+        self.cost.risk_adj_per_lot = pd.Series(0.0, index=self.position.index)
+        self.cost.return_series = pd.Series(0.0, index=self.position.index)